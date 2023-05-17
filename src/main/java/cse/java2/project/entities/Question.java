025ac57a037d2d73b68467e446acdc8dd308b4b5
AndyCheng <100212381+andychenggg@users.noreply.github.com>
2023-05-18 00:58:41 +0800

    建了相应的实体

---
+++ src/main/java/cse/java2/project/entities/Question.java
@@ -0,0 +1,20 @@
+package cse.java2.project.entities;
+
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Question {
+    private List<String> tags;
+    private Owner owner;
+    private boolean is_answered;
+    private int view_count;
+    private int answer_count;
+    private int score;
+    private long last_activity_date;
+    private long creation_date;
+    private long last_edit_date;
+    private long question_id;
+    private String content_license;
+    private String link;
+    private String title;
+}
