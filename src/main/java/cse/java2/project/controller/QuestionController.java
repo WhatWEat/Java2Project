14e3720560892b3419d0456981170a2e8ff5a1c3
AndyCheng <100212381+andychenggg@users.noreply.github.com>
2023-05-29 19:40:16 +0800

    craw the answers.json

--- src/main/java/cse/java2/project/controller/QuestionController.java
+++ src/main/java/cse/java2/project/controller/QuestionController.java
@@ -6,6 +6,7 @@ import cse.java2.project.Mapper.OwnerMapper;
 import cse.java2.project.Mapper.QuestionMapper;
 import cse.java2.project.entities.Owner;
 import cse.java2.project.entities.Question;
+import cse.java2.project.util.JsonParser;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -21,41 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QuestionController {
-    @Autowired
-    private QuestionMapper questionMapper;
-    @Autowired
-    private OwnerMapper ownerMapper;
 
-    @GetMapping("/import")
-    public String importData() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("./src/main/resources/jsons/questions.json")));
-        String line;
-        StringBuilder response = new StringBuilder();
 
-        while ((line = reader.readLine()) != null) {
-            response.append(line).append('\n');
-        }
-        reader.close();
-        JSONArray itemsArray = JSON.parseArray(response.toString());
-        HashSet<Long> account_id_map = new HashSet<>();
-        HashSet<Long> question_id_map = new HashSet<>();
-        List<Question> questions = itemsArray.toJavaList(Question.class);
-        for (Question q: questions) {
-            Owner owner = q.getOwner();
-            if(!account_id_map.contains(owner.getAccount_id())){
-                ownerMapper.insert(owner);
-                account_id_map.add(owner.getAccount_id());
-            }
-            if(!question_id_map.contains(q.getQuestion_id())){
-                q.setOwner_account_id(owner.getAccount_id());
-                questionMapper.insert(q);
-                q.getTags().forEach(t -> {
-                    questionMapper.insertQuestionTag(q.getQuestion_id(), t);
-                });
-                question_id_map.add(q.getQuestion_id());
-            }
-
-        }
-        return "success";
+    @GetMapping("/NumberOfAnswers/Q1")
+    public float noAnsCount(){
+        return JsonParser.noAnsCount();
     }
+
 }
