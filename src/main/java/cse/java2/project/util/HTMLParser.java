e7d5deaf9b3b6631e6ac156e5d9d24de49f079a5
AndyCheng <100212381+andychenggg@users.noreply.github.com>
2023-05-30 17:56:02 +0800

    完成了advance

--- src/main/java/cse/java2/project/util/HTMLParser.java
+++ src/main/java/cse/java2/project/util/HTMLParser.java
@@ -10,8 +10,10 @@ import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.jsoup.Jsoup;
@@ -43,26 +45,28 @@ public class HTMLParser {
             CompilationUnit cu;
             try {
                 cu = StaticJavaParser.parse(codeBlock);
+                Set<String> names = new HashSet<>();
                 cu.accept(new VoidVisitorAdapter<Void>() {
                     @Override
                     public void visit(ClassOrInterfaceDeclaration n, Void arg) {
-                        int value = result.getOrDefault(n.getName().toString(), 0);
-                        result.put(n.getName().toString(), value+1);
+                        names.add(n.getNameAsString());
                         super.visit(n, arg);
                     }
                     @Override
                     public void visit(MethodDeclaration n, Void arg) {
-                        int value = result.getOrDefault(n.getName().toString(), 0);
-                        result.put(n.getName().toString(), value+1);
+                        names.add(n.getNameAsString());
                         super.visit(n, arg);
                     }
                     @Override
                     public void visit(NameExpr n, Void arg) {
-                        int value = result.getOrDefault(n.getName().toString(), 0);
-                        result.put(n.getName().toString(), value+1);
+                        names.add(n.getNameAsString());
                         super.visit(n, arg);
                     }
                 }, null);
+                names.forEach(n -> {
+                    int value = result.getOrDefault(n, 0);
+                    result.put(n, value+1);
+                });
 
             }catch (Exception e) {
                 System.out.println("无法解析的代码块: " + codeBlock);
